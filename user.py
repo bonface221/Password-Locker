a51aa0e41b4d361d02c006cf254213d4fa8995b3
[bonface221] <[mainaboniface221@gmail.com]>
2022-04-24 23:55:14 +0300

    ft:made changes by adding the docstrings

--- user.py
+++ user.py
@@ -1,15 +1,28 @@
 class User:
+    '''
+    Class that generates new instances of contacts
+    '''
+
     user_list =[]
+    
+    '''
+    __init__ method that helps us define properties for our objects.
+    '''
     def __init__(self,first_name,last_name, user_name,password):
         self.fist_name =first_name
         self.last_name = last_name
         self.user_name = user_name
         self.password= password
         
-
+    '''
+    method to append users to the user list
+    '''
     def save_user(self):
         User.user_list.append(self)
 
+    '''
+    method to find users by the name for validation purposes
+    '''
     @classmethod
     def find_user_by_username(cls,name):
         for user in cls.user_list:
@@ -18,6 +31,9 @@ class User:
         
         return False
 
+    '''
+    method to validate a user using the password find
+    '''
     @classmethod 
     def find_user_by_password(cls, password):
         for user in cls.user_list:
